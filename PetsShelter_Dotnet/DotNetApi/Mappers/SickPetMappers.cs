1902a59dd0f50ff3873b72c254ff460fc09224b7
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2024-05-22 17:35:48 +0200

    Create SickPetMappers

---
+++ PetsShelter_Dotnet/DotNetApi/Mappers/SickPetMappers.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetApi.Dtos.SickPet;
+using DotNetApi.Models;
+
+namespace DotNetApi.Mappers
+{
+    public static class SickPetMappers
+    {
+         public static SickPetDto ToSickPetDto(this SickPet pet)
+        {
+            return new SickPetDto
+            {
+                Id = pet.Id,
+                Name = pet.Name,
+                Species = pet.Species,
+                Disease = pet.Disease,
+                CurrentTokens = pet.CurrentTokens,
+                RequiredTokens = pet.RequiredTokens,
+                Status = pet.Status,
+                PhotoPath = pet.PhotoPath
+            };
+        }
+
+        public static SickPet ToPetFromCreateDto(this CreateSickPetDto createSickPetDto, string photoPath)
+        {
+            return new SickPet{
+                Name = createSickPetDto.Name,
+                Species = createSickPetDto.Species,
+                Disease = createSickPetDto.Disease,
+                RequiredTokens = createSickPetDto.RequiredTokens,
+                PhotoPath = photoPath
+            };
+        }
+    }
+}
