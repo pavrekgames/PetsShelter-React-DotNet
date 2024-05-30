8e2901f1b968716a6cc08fab08e3ae5c7e959197
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2024-05-30 16:01:39 +0200

    Implement Mail Service

--- PetsShelter_Dotnet/DotNetApi/Interfaces/IMailService.cs
+++ PetsShelter_Dotnet/DotNetApi/Interfaces/IMailService.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotNetApi.Mail;
 
 namespace DotNetApi.Interfaces
 {
     public interface IMailService
     {
-        
+        Task<bool> SendMailAsync(MailData mailData);
     }
 }
--- PetsShelter_Dotnet/DotNetApi/Services/MailService.cs
+++ PetsShelter_Dotnet/DotNetApi/Services/MailService.cs
@@ -3,11 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DotNetApi.Interfaces;
+using DotNetApi.Mail;
+using MailKit.Net.Smtp;
+using MimeKit;
 
 namespace DotNetApi.Services
 {
-    public class MailService: IMailService
+    public class MailService : IMailService
     {
-        
+
+        private readonly MailSettings mailSettings;
+
+        public MailService(MailSettings mailSettings)
+        {
+            this.mailSettings = mailSettings;
+        }
+
+        public async Task<bool> SendMailAsync(MailData mailData)
+        {
+            try
+            {
+                using (var emailMessage = new MimeMessage())
+                {
+                    MailboxAddress emailFrom = new MailboxAddress(mailSettings.SenderName, mailSettings.SenderEmail);
+                    emailMessage.From.Add(emailFrom);
+                    MailboxAddress emailTo = new MailboxAddress(mailData.EmailToName, mailData.EmailToId);
+                    emailMessage.To.Add(emailTo);
+
+                    emailMessage.Cc.Add(new MailboxAddress("Cc Receiver", "cc@example.com"));
+                    emailMessage.Bcc.Add(new MailboxAddress("Bcc Receiver", "bcc@example.com"));
+
+                    emailMessage.Subject = mailData.EmailSubject;
+
+                    BodyBuilder emailBodyBuilder = new BodyBuilder();
+                    emailBodyBuilder.TextBody = mailData.EmailBody;
+
+                    emailMessage.Body = emailBodyBuilder.ToMessageBody();
+                    //this is the SmtpClient from the Mailkit.Net.Smtp namespace, not the System.Net.Mail one
+                    using (SmtpClient mailClient = new SmtpClient())
+                    {
+                        mailClient.Connect(mailSettings.Server, mailSettings.Port, MailKit.Security.SecureSocketOptions.StartTls);
+                        mailClient.Authenticate(mailSettings.UserName, mailSettings.Password);
+                        mailClient.Send(emailMessage);
+                        mailClient.Disconnect(true);
+                    }
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
     }
 }
